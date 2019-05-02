60063956cac8d654a6e5b7c7c3e7641e654d2601
Steve Gadoury <steve.gadoury@gmail.com>
2019-05-01 20:09:21 -0400

    change port

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var routes = require("./routes/routes.js")(app);
 
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
     console.log("Listening on port %s...", server.address().port);
 });
